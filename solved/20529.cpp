3ad233e3cfa4b022788ffb3550818643e254f17a
Honghyun Ahn <32119804+hyunynim@users.noreply.github.com>
2021-01-07 17:24:33 +0900

    Create 20529.cpp

---
+++ solved/20529.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+const char * mbti[] = { "ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP", "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ" };
+int d[16][16][16];
+int dist(int a, int b, int c) {
+	int res = 0;
+	for (int i = 0; i < 4; ++i) {
+		res += (mbti[a] != mbti[b]);
+		res += (mbti[b] != mbti[c]);
+		res += (mbti[c] != mbti[a]);
+	}
+	return res;
+}
+map<string, int> comp;
+vector<int> cnt;
+int ans;
+void go(int toPick, vector<int> & pick) {
+	if (toPick == 0) {
+		ans = max(ans, d[pick[0]][pick[1]][pick[2]]);
+		return;
+	}
+	for (int i = 0; i < 16; ++i) {
+		if (cnt[i]) {
+			--cnt[i];
+			pick.push_back(i);
+			go(toPick - 1, pick);
+			pick.pop_back();
+			++cnt[i];
+		}
+	}
+}
+int main() {
+	for (int i = 0; i < 16; ++i) {
+		comp[mbti[i]] = i;
+		for (int j = 0; j < 16; ++j)
+			for (int k = 0; k < 16; ++k)
+				d[i][j][k] = dist(i, j, k);
+	}
+
+	int t; scanf("%d", &t);
+	while (t--) {
+		int n; scanf("%d", &n);
+		char msg[11];
+		cnt.resize(16);
+		for (int i = 0; i < n; ++i) {
+			scanf("%s", msg);
+			++cnt[comp[msg]];
+		}
+		for (int i = 0; i < 16; ++i)
+			cnt[i] = min(3, cnt[i]);
+
+		vector<int> tmp;
+		go(3, tmp);
+
+		printf("%d\n", ans);
+		ans = 0;
+		cnt.clear();
+
+	}
+}
